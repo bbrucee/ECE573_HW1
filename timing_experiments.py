5446c824bdee70e56e0b4f9a5d74be763de9ff31
Brendan <abae97@gmail.com>
2019-02-14 15:13:11 -0500

    added skeleton for timing in a file named timing_experiments.py

---
+++ timing_experiments.py
@@ -0,0 +1,22 @@
+import Q1.Q1
+import Q2.Q2
+import Q4.Q4
+import Q5.Q5
+from glob import glob
+from time import time
+
+
+def three_sum_timing():
+    start_time = time()
+    # Stuff here
+    end_time = time()
+    time_elasped = end_time - start_time
+    pass
+
+
+def union_find_timing():
+    pass
+
+
+def farthest_pair_timing():
+    pass
